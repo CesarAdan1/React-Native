1dff357c2a4d8e1aee60994d0b2d08b47c5e9764
CesarAdan1 <whoclash@hotmail.com>
2019-01-31 15:40:22 -0600

    scrollableView

--- src/Components/AlbumDetail.js
+++ src/Components/AlbumDetail.js
@@ -5,34 +5,68 @@ import Card from './Card';
 
 //Destructuring
 const AlbumDetail = ({ album }) => {
-    const {title, artist, thumbnail_image} = album;
-    const { thumbnailStyle, headerContentStyle } = styles;
+    const {
+        title, 
+        artist, 
+        image,
+        thumbnail_image
+    } = album;
+
+    const { 
+        thumbnailStyle, 
+        headerContentStyle, 
+        thumbnailContainerStyle, 
+        headerTextStyle, 
+        imageStyle
+    } = styles;
+
     return (
         <Card>
             <CardSection>
-                <View>
+                <View style={thumbnailContainerStyle}>
                     <Image 
                         style={thumbnailStyle}
                         source={{ uri: thumbnail_image }}
                     />
                 </View>
                 <View style={headerContentStyle}>
-                    <Text>{title}</Text>
+                    <Text style={headerTextStyle}>{title}</Text>
                     <Text>{artist}</Text>
                 </View>
             </CardSection>
-        </Card>     
+            <CardSection>
+                <Image 
+                    style={imageStyle}
+                    source={{ uri: image }}/>
+            </CardSection>
+        </Card>      
     );
 };
+
 
 const styles = {
     headerContentStyle: {
         flexDirection: 'column',
         justifyContent: 'space-around'
     },
+    headerTextStyle: {
+        fontSize: 18,
+    },
     thumbnailStyle: {
         height: 50,
         width: 50 
+    },
+    thumbnailContainerStyle: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft: 10,
+        marginRight: 10
+    },
+    //trick react native for styling  flex and with width: null 
+    imageStyle: {
+        height: 300,
+        flex: 1,
+        width: null
     }
 }
 
--- src/Components/AlbumList.js
+++ src/Components/AlbumList.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { ScrollView } from 'react-native';
 import AlbumDetail from './AlbumDetail';
 import axios from 'axios';
 
@@ -33,12 +33,12 @@ class AlbumList extends Component {
   render() {
       console.log(this.state);
     return (
-    <View 
+    <ScrollView
         style={styles}
     >
         {/*//Put the state,call the function renderAlbums*/}
         {this.renderAlbums()}
-    </View>
+    </ScrollView>
     )
   }
 }
