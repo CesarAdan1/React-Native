3a3db63f845890b81c3e0b17bcf950e5574c6c91
CesarAdan1 <whoclash@hotmail.com>
2019-01-31 14:52:46 -0600

    destructuringandrefactorincodealbumdetailComponent

--- src/Components/AlbumDetail.js
+++ src/Components/AlbumDetail.js
@@ -1,17 +1,39 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, Image } from 'react-native';
 import CardSection from './CardSection';
 import Card from './Card';
 
-
-const AlbumDetail = (props) => {
+//Destructuring
+const AlbumDetail = ({ album }) => {
+    const {title, artist, thumbnail_image} = album;
+    const { thumbnailStyle, headerContentStyle } = styles;
     return (
         <Card>
             <CardSection>
-                <Text>{props.album.title}</Text>
+                <View>
+                    <Image 
+                        style={thumbnailStyle}
+                        source={{ uri: thumbnail_image }}
+                    />
+                </View>
+                <View style={headerContentStyle}>
+                    <Text>{title}</Text>
+                    <Text>{artist}</Text>
+                </View>
             </CardSection>
         </Card>     
     );
+};
+
+const styles = {
+    headerContentStyle: {
+        flexDirection: 'column',
+        justifyContent: 'space-around'
+    },
+    thumbnailStyle: {
+        height: 50,
+        width: 50 
+    }
 }
 
 export default AlbumDetail; 
